fe14caf39d79d1cacd98ea63ddb0035495eb974e
morimolymoly <morimolymoly@gmail.com>
2021-07-04 01:45:20 +0900

    make use of framebuffer2

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,7 +7,14 @@
 use core::panic::PanicInfo;
 
 #[no_mangle]
-extern "efiapi" fn kernel_main() -> ! {
+extern "efiapi" fn kernel_main(frame_buffer_base: u64, frame_buffer_size: u64) -> ! {
+    let frame_buffer_start = frame_buffer_base as *mut u8;
+    let frame_buffer = unsafe { core::slice::from_raw_parts_mut(frame_buffer_start, frame_buffer_size as usize)};
+    
+    for i in 0 .. frame_buffer_size as usize {
+        frame_buffer[i] = (i as u8) % 255;
+    }
+    
     loop{
         unsafe {
             asm!("hlt");
