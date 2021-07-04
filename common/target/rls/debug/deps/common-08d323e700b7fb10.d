143b4ee3700b55433b3974cb0666c4583ed4ef51
morimolymoly <morimolymoly@gmail.com>
2021-07-04 13:33:34 +0900

    remove target from common

--- common/target/rls/debug/deps/common-08d323e700b7fb10.d
+++
@@ -1,5 +0,0 @@
-/Users/moly/work/mikan/common/target/rls/debug/deps/common-08d323e700b7fb10.rmeta: src/lib.rs
-
-/Users/moly/work/mikan/common/target/rls/debug/deps/common-08d323e700b7fb10.d: src/lib.rs
-
-src/lib.rs:
